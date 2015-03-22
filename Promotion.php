14192042213443e52aac9630ba7f2a28573a3aa3
Aaron Mitchell <amitchell@cogolabs.com>
2015-03-22 10:53:14 -0400

    initial commit

---
+++ Promotion.php
@@ -0,0 +1,55 @@
+<html>
+<head>
+<meta name="viewport" content="width=device-width, user-scalable=no" />
+<title>Afghan: Portland Maine Punk Rock</title>
+<link href='http://fonts.googleapis.com/css?family=Germania+One' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="afghan.css">
+<style>
+form {
+width: 300px;
+background: #eee;
+color: #000;
+margin-left: auto;
+margin-right: auto;
+padding: 10px;
+}
+label {
+display: block;
+margin-bottom: 5px;
+}
+input {
+display: block;
+width: 100%;
+margin-bottom: 10px;
+font-size: 16px;
+padding: 4px;
+}
+.submitButton {
+margin-top: 20px;
+background: #e35d14;
+color: #fff;
+font-size: 24px;
+font-weight: bold;
+border: 0;
+border-radius: 10px;
+padding: 10px 0;
+}
+</style>
+</head>
+
+<body>
+<form action="form.php" method="post" />
+<label for="First_Name">First Name:</label>
+<input type="text" name="First_Name" />
+<label for="Last_Name">Last Name:</label>
+<input type="text" name="Last_Name" />
+<label for="Email">Email:</label>
+<input type="text" name="Email" />
+<label for="Zip_Code">Zip Code:</label>
+<input type="text" name="Zip_Code" />
+
+<input type="submit" class="submitButton" value="Free Download" />
+</form>
+
+</body>
+</html>
---
+++ form.php
@@ -0,0 +1,19 @@
+<?PHP
+
+$user_name = "manscann_manscann_afghan";
+$password = "Nightpanther1";
+$database = "manscann_manscann_afghan";
+$server = "localhost";
+
+mysql_connect($server, $user_name, $password);
+$db_found = mysql_select_db($database);
+if ($db_found) {
+	print "Database Found";
+}
+else {
+	print "Database NOT Found";
+}
+
+?>
+
+
